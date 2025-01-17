978d06cbdb754913e21ac330ddd290286744b093
Urmi Dedhia <udedhia@andrew.cmu.edu>
2025-01-16 21:06:59 -0500

    added API endpoint

--- analyze.py
+++ analyze.py
@@ -2,9 +2,13 @@ from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from msrest.authentication import CognitiveServicesCredentials
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 import time
+import os
+from dotenv import load_dotenv
 
-endpoint = "ENTER ENDPOINT HERE"
-key = "ENTER KEY HERE"
+load_dotenv()
+
+endpoint = os.environ["ENDPOINT"]
+key = os.environ["KEY"]
 
 credentials = CognitiveServicesCredentials(key)
 
